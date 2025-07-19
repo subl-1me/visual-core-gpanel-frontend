efab7c580bf6ae9f3e94259e51f12f05edc777f2
subl-1me <julio.ugalde404@gmail.com>
2025-07-19 13:44:34 -0600

    Add shirt layout

---
+++ src/app/components/add-shirt/add-shirt.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-shirt',
+  imports: [],
+  templateUrl: './add-shirt.component.html',
+  styleUrl: './add-shirt.component.css',
+})
+export class AddShirtComponent {}

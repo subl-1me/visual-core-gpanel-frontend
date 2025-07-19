1fa70ce503bf118495c56f42ce9f2ca4970a2244
subl-1me <julio.ugalde404@gmail.com>
2025-07-19 13:26:59 -0600

    Dashboard - added main router ~ added conts tab names

---
+++ src/app/const.ts
@@ -0,0 +1,12 @@
+// sidebar navigation tab names
+export const TAB_NAMES = {
+  DASHBOARD: '',
+  ADD_SHIRT: 'add-shirt',
+  STOCK: 'stock',
+  SALES: 'sales',
+  CUSTOMERS: 'customers',
+  HISTORY: 'history',
+  PROVIDERS: 'providers',
+  CONFIG: 'config',
+  PROFILE: 'profile',
+};
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { TAB_NAMES } from '../../const';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   imports: [],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
+  constructor(private router: Router) {}
 
+  public TAB_NAMES = TAB_NAMES;
+  public toggleActiveTab(tabName: string): void {
+    this.router.navigate([`${tabName}`]);
+  }
 }

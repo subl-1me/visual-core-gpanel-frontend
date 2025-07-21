6da98ebf711e2b6c838e47462d306fc2ecf66214
subl-1me <julio.ugalde404@gmail.com>
2025-07-21 03:01:04 -0600

    Login - Secure implementation of validation for adms

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import Admin from '../../models/admin';
+import { AuthenticationService } from '../../services/authentication/authentication.service';
+import { FormsModule, Form } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, NgIf],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  public isLoading: boolean = false;
+  public errorLoginMessage: string = '';
 
+  public user: Admin = {
+    id: '',
+    username: '',
+    email: '',
+  };
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+  ) {}
+
+  public login(form: Form): void {
+    this.isLoading = true;
+    this.authService.requestAuthentication(this.user).subscribe((response) => {
+      const { error, authenticated, session, message } = response.authResponse;
+      if (error || !authenticated) {
+        this.errorLoginMessage = message;
+        this.isLoading = false;
+        return;
+      }
+
+      this.authService.saveLoginSession(session);
+      this.isLoading = false;
+      this.router.navigate(['']);
+    });
+  }
 }
--- src/app/services/authentication/authentication.service.ts
+++ src/app/services/authentication/authentication.service.ts
@@ -1,13 +1,36 @@
 import { Injectable } from '@angular/core';
+import Admin from '../../models/admin';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
-  constructor() {}
+  private headers: HttpHeaders = new HttpHeaders().set(
+    'content-type',
+    'application/json'
+  );
+  constructor(private httt: HttpClient) {}
 
   public isAuth(): boolean {
-    const authTkn = localStorage.getItem('tkn');
-    return authTkn ? true : false;
+    const session = this.getLoginSession();
+    return session ? true : false;
+  }
+
+  public requestAuthentication(user: Admin): Observable<any> {
+    return this.httt.post(environment.apiUrl + '/login', user, {
+      headers: this.headers,
+    });
+  }
+
+  public saveLoginSession(session: any): void {
+    localStorage.setItem('session', JSON.stringify(session));
+  }
+
+  public getLoginSession(): any {
+    const session = JSON.parse(localStorage.getItem('session') || '{}');
+    return session.id ? session : null;
   }
 }

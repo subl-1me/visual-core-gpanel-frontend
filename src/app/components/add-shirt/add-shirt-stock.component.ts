1de04a6459eebce7c0ed81007b9078f352fc7522
subl-1me <julio.ugalde404@gmail.com>
2025-07-24 13:07:04 -0600

    Add shirt stock - Added Stock Form validations

--- src/app/components/add-shirt/add-shirt-stock.component.ts
+++ src/app/components/add-shirt/add-shirt-stock.component.ts
@@ -3,8 +3,9 @@ import { Router } from '@angular/router';
 import { TIER_NAMES } from '../../const';
 import { NgFor, NgIf } from '@angular/common';
 import Stock from '../../models/stock';
-import { FormsModule, Form } from '@angular/forms';
+import { FormsModule, Form, NgForm } from '@angular/forms';
 import { StockService } from '../../services/stock/stock.service';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-add-shirt',
@@ -13,19 +14,23 @@ import { StockService } from '../../services/stock/stock.service';
   styleUrl: './add-shirt-stock.component.css',
 })
 export class AddShirtStockComponent {
+  @ViewChild('createStockForm') form!: NgForm;
+
   public tiers: any = TIER_NAMES;
   public availableColors: string[] = [];
   public stock: Stock;
   public selectedColor: string = '#fff';
   public lastSelectedColor: string;
   public creationStatusMessage: string = '';
+  public isLoading: boolean = false;
+  public responseMessage: string = '';
 
   constructor(private router: Router, private stockService: StockService) {
     this.lastSelectedColor = this.selectedColor;
     this.stock = {
       status: 'Available',
       sizes: [
-        { size: 'L', quantity: 0 },
+        { size: 'S', quantity: 0 },
         { size: 'M', quantity: 0 },
         { size: 'L', quantity: 0 },
         { size: 'XL', quantity: 0 },
@@ -34,7 +39,7 @@ export class AddShirtStockComponent {
         name: '',
         description: '',
         imageUrl: '',
-        price: 0,
+        price: 100,
         tier: '',
         media: [],
       },
@@ -47,8 +52,43 @@ export class AddShirtStockComponent {
     this.router.navigate(['/stock']);
   }
 
-  public createStock(_form: Form): void {
-    this.creationStatusMessage = 'Saving stock...';
+  public check(): void {
+    if (this.stock.details.price < 100) {
+      this.stock.details.price = 100;
+    }
+  }
+
+  public checkStockValue(stockType: string): void {
+    switch (stockType) {
+      case 'S':
+        if (this.stock.sizes[0].quantity < 0) {
+          this.stock.sizes[0].quantity = 0;
+        }
+        break;
+      case 'M':
+        if (this.stock.sizes[1].quantity < 0) {
+          this.stock.sizes[1].quantity = 0;
+        }
+        break;
+      case 'L':
+        if (this.stock.sizes[2].quantity < 0) {
+          this.stock.sizes[2].quantity = 0;
+        }
+        break;
+      case 'XL':
+        if (this.stock.sizes[3].quantity < 0) {
+          this.stock.sizes[3].quantity = 0;
+        }
+        break;
+    }
+  }
+
+  public createStock(): void {
+    if (!this.form.valid) {
+      return;
+    }
+
+    this.isLoading = true;
     this.stock.availableColors = [...this.availableColors];
 
     // calculate total
@@ -57,12 +97,24 @@ export class AddShirtStockComponent {
     }, 0);
 
     this.stock.total = stockSum;
-    this.stockService.add(this.stock).subscribe((response) => {
-      console.log(response);
+    this.stockService.add(this.stock).subscribe({
+      next: (_response) => {
+        this.isLoading = false;
+        this.responseMessage = '';
+        this.router.navigate(['/stock']);
+      },
+      error: (err) => {
+        const { error } = err;
+        this.responseMessage = `Error trying to add new stock: ${error.message}`;
+        this.isLoading = false;
+      },
     });
   }
 
   public addSelectedColorList(color: string, event?: any) {
+    if (this.availableColors.includes(color)) {
+      return;
+    }
     if (event) {
       const colorPickerInput = document.getElementById(
         'color-selector'
@@ -79,6 +131,12 @@ export class AddShirtStockComponent {
     }
 
     return;
+  }
+
+  public removeColor(hex: string): void {
+    this.availableColors = this.availableColors.filter(
+      (color) => color !== hex
+    );
   }
 
   private changeColorPickerView(): void {

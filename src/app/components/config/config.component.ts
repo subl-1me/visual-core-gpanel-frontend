f92d946afa5e10035b47391a8466cb343440b2ed
subl-1me <julio.ugalde404@gmail.com>
2025-08-17 13:45:41 -0600

    Config ~ Added loader spiner for Admin List

--- src/app/components/config/config.component.ts
+++ src/app/components/config/config.component.ts
@@ -2,19 +2,20 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import Admin from '../../models/admin';
 import { ConfigService } from '../../services/config/config.service';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { OnInit } from '@angular/core';
 import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-config',
-  imports: [NgFor, DatePipe],
+  imports: [NgFor, DatePipe, NgIf],
   templateUrl: './config.component.html',
   styleUrl: './config.component.css',
 })
 export class ConfigComponent implements OnInit {
   public adminList: Admin[] = [];
   public isLoadingAdminList: boolean = false;
+  public displayedAdmins: Admin[] = [];
 
   constructor(private router: Router, private configService: ConfigService) {}
 
@@ -28,18 +29,26 @@ export class ConfigComponent implements OnInit {
 
   private getAdminList(): void {
     this.isLoadingAdminList = true;
-    this.configService.getAdminList().subscribe((response) => {
-      this.adminList = [...response.users].map((item) => {
-        return {
-          username: item.username,
-          id: item.id,
-          name: item.name,
-          lastName: item.lastname,
-          updatedAt: item.updatedAt,
-          createdAt: item.createdAt,
-          email: item.email,
-        };
-      });
+    this.configService.getAdminList().subscribe({
+      next: (response) => {
+        this.adminList = [...response.users].map((item) => {
+          return {
+            username: item.username,
+            id: item.id,
+            name: item.name,
+            lastName: item.lastname,
+            updatedAt: item.updatedAt,
+            createdAt: item.createdAt,
+            email: item.email,
+          };
+        });
+        this.displayedAdmins = [...this.adminList];
+
+        this.isLoadingAdminList = false;
+      },
+      error: (err) => {
+        this.isLoadingAdminList = false;
+      },
     });
   }
 }
